8579e6609432568238c068338e1b7531fe1b59d6
DJ Viking <anders.swedishviking@gmail.com>
2020-10-25 02:55:54 -0400

    changing image links to be https

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -44,7 +44,7 @@ const SearchBooks = () => {
         authors: book.volumeInfo.authors || ['No author to display'],
         title: book.volumeInfo.title,
         description: book.volumeInfo.description,
-        image: book.volumeInfo.imageLinks?.thumbnail || '',
+        image: book.volumeInfo.imageLinks?.thumbnail.replace(/http/, 'https') || '',
         link: book.volumeInfo.canonicalVolumeLink
       }));
       console.log(bookData);
